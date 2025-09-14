53de6c4d606c36b61c50ce29c26ff78bc7268c5a
sarrow04 <165770207+sarrow04@users.noreply.github.com>
2025-09-14 11:53:27 +0900

    Update script.js

--- script.js
+++ script.js
@@ -183,7 +183,7 @@ function clearUIForNewQuestion() {
 function displayHintImage(question) {
     if (question.image && question.image.trim() !== '') {
         // (変更後)
-hintImage.src = `../mages/${question.image}`;
+hintImage.src = `images/${question.image}`;
         hintImage.style.display = 'block';
     } else {
         hintImage.style.display = 'none';
